c33e2040028002b47ced871c6650389efa97f3eb
Administrator <bineawu@163.com>
2022-05-11 18:33:54 +0800

    feat: Memoizing Selector Functions

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import {configureStore} from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/PostsSlice'
 import userReducer from '../features/users/UserSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: userReducer,
+    notifications: notificationsReducer,
   },
 })
--- src/features/users/UserSlice.js
+++ src/features/users/UserSlice.js
@@ -9,7 +9,7 @@ export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
 })
 
 const userSlice = createSlice({
-  name: 'user',
+  name: 'users',
   initialState,
   reducers: {},
   extraReducers(builder) {
@@ -20,3 +20,8 @@ const userSlice = createSlice({
 })
 
 export default userSlice.reducer
+
+export const selectAllUsers = (state) => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
